cabd491f47e4a793417ae019ce83d454846f86c9
BALMINE Davy <PFTS611@corp.log.intra.laposte.fr>
2018-06-20 12:24:14 +0200

    First Commit

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdvertController extends Controller
+{
+    public function indexAction()
+    {
+        $content = $this->get('templating')
+        ->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Gosuhan'))
+        ;
+
+        return new Response($content);
+    }
+
+    public function viewAction($id)
+    {
+        return new Response("Affichage de l'annonce d'id : " .$id);
+    }
+
+    public function viewSlugAction($slug, $year, $format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '".$slug."', créée en ".$year." et au format ".$format."."
+        );
+    }
+}
